d27e382c94fbebb1a24713a739c77fc81ff4532c
Pocento <cristiancid88@gmail.com>
2021-04-06 11:26:55 -0400

    semana4 clase2

---
+++ js/mijavascript.js
@@ -0,0 +1,15 @@
+//variables
+alert('INICIO')
+var numero =1;
+var numero2 =2;
+var palabra = "hola mundo";
+var isAuto= true;
+
+var resp = palabra+" " + numero2;
+var resp2 = numero +" "+numero2+" "+palabra+" "+isAuto;
+
+
+console.log("Resultado: "+resp);
+console.log("Resultado: "+resp2);
+alert(resp)
+document.getElementById("titulo_jumbo")
